f6b7fea5f7469c56f77fc349b99be690b7265352
FOODOHOLIC <adarshkohli183@gmail.com>
2025-02-03 21:48:16 +0530

    Initial commit

---
+++ Web3/hardhat.config.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import '@nomiclabs/hardhat-ethers';
+
+dotenv.config();
+
+export default {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      gasPrice: 'auto',
+      chainId: 11155111,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    // subnet: {
+    //   url: process.env.NODE_URL,
+    //   chainId: Number(process.env.CHAIN_ID),
+    //   gasPrice: 'auto',
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
+  },
+}
---
+++ Web3/scripts/deploy.ts
@@ -0,0 +1,32 @@
+import hre from 'hardhat';
+import { ethers } from 'ethers';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+const _metadataUri = 'https://gateway.pinata.cloud/ipfs/QmX2ubhtBPtYw75Wrpv6HLb1fhbJqxrnbhDo1RViW3oVoi';
+
+async function deploy(name: string, ...params: string[]) {
+  const contractFactory = await hre.ethers.getContractFactory(name); // Use hre.ethers
+  const contract = await contractFactory.deploy(...params);
+  await contract.deployed();
+  return contract;
+}
+
+async function main() {
+  const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+
+  console.log(`Deploying smart contract from: ${wallet.address}`);
+
+  const AVAXGods = await deploy('AVAXGods', _metadataUri);
+  
+  console.log(`✅ Contract deployed at: ${AVAXGods.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(`❌ Deployment failed:`, error);
+    process.exit(1);
+  });
